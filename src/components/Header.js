d63d0fc362690c2c7903f25f3f88843831b68385
Alexandre Zagame <zagamealexandre@gmail.com>
2021-01-11 17:28:12 +0100

    adding parallax effect on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import Typed from "react-typed";
 
 const Header = () => {
+
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    function handleScroll() {
+      setOffset(window.pageYOffset);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [offset]);
+
   return (
     <div className="header-wraper">
+
+      <img
+          src="https://images.unsplash.com/photo-1609342122563-a43ac8917a3a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80"
+          alt="test"
+          className="parallax"
+          style={{
+            filter: `blur(4px)`,
+            transform: `translateY(${offset * 0.5}px)`
+          }}
+        />
+
       <div className="main-info">
         <h1>Alexandre Zagame</h1>
         <Typed 
