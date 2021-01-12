cfa546ad27e000acdf8143767df75fdc36f98f15
Alexandre Zagame <zagamealexandre@gmail.com>
2021-01-12 14:08:17 +0100

    contact component and error handling completed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,62 +1,116 @@
-import React from "react";
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
+import { useForm } from "react-hook-form";
 
 const Contact = () => {
+  const [successMessage, setSuccessMessage] = useState("");
+  const { register, handleSubmit, errors } = useForm();
+
+  const serviceID = "service_ID";
+  const templateID = "template_ID";
+  const userID = "user_wIQVoVOuFRefoGUuTYwwH";
+
+  const onSubmit = (data, r) => {
+    sendEmail(
+      serviceID,
+      templateID,
+      {
+        name: data.name,
+        email: data.email,
+        subject: data.subject,
+        description: data.description,
+      },
+      userID
+    );
+    r.target.reset();
+  };
+
+  const sendEmail = (serviceID, templateID, variables, userID) => {
+    emailjs
+      .send(serviceID, templateID, variables, userID)
+      .then(() => {
+        setSuccessMessage("Form sent successfully.");
+      })
+      .catch((err) => console.error(`Something went wrong ${err}`));
+  };
+
   return (
     <div className="contact">
       <div className="text-center">
         <h1>Contact me</h1>
         <p>Reach out for further information or get in touch on Linkedin.</p>
+  <span className="success-message">{successMessage}</span>
       </div>
       <div className="container">
-        <div className="row">
-          <div className="col-md-6 col-xs-12">
-            <div className="text-center">
-              <input
-                id="name"
-                type="text"
-                className="form-control"
-                placeholder="Name"
-                name="name"
-              />
-              <div className="line"></div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="row">
+            <div className="col-md-6 col-xs-12">
+              <div className="text-center">
+                <input
+                  id="name"
+                  type="text"
+                  className="form-control"
+                  placeholder="Name"
+                  name="name"
+                  ref={register({
+                    required: "Please enter your name",
+                  })}
+                />
+                <div className="line"></div>
+              </div>
+              <span className="error-message">
+                {errors.name && errors.name.message}
+              </span>
+              <div className="text-center">
+                <input
+                  id="email"
+                  type="text"
+                  className="form-control"
+                  placeholder="Email"
+                  name="email"
+                  ref={register({
+                    required: "Please enter your email",
+                  })}
+                />
+                <div className="line"></div>
+              </div>
+              <span className="error-message">
+                {errors.email && errors.email.message}
+              </span>
+              <div className="text-center">
+                <input
+                  id="subject"
+                  type="text"
+                  className="form-control"
+                  placeholder="Subject"
+                  name="subject"
+                  ref={register({
+                    required: "Please enter the subject",
+                  })}
+                />
+                <div className="line"></div>
+              </div>
+              <span className="error-message">
+                {errors.subject && errors.subject.message}
+              </span>
             </div>
-            <div className="text-center">
-              <input
-                id="email"
-                type="text"
-                className="form-control"
-                placeholder="Email"
-                email="email"
-              />
-              <div className="line"></div>
-            </div>
-            <div className="text-center">
-              <input
-                id="subject"
-                type="text"
-                className="form-control"
-                placeholder="Subject"
-                subject="subject"
-              />
-              <div className="line"></div>
+            <div className="col-md-6 col-xs-12">
+              <div className="text-center">
+                <textarea
+                  id="description"
+                  type="text"
+                  className="form-control"
+                  placeholder="Feel free to write here or to add me on Linkedin..."
+                  name="description"
+                ></textarea>
+                <div className="line"></div>
+              </div>
+              <button className="btn-main-offer contact-btn" type="submit">
+                Contact me
+              </button>
             </div>
           </div>
-          <div className="col-md-6 col-xs-12">
-            <div className="text-center">
-              <textarea
-                id="description"
-                type="text"
-                className="form-control"
-                placeholder="Feel free to write here or to add me on Linkedin..."
-                description="description"
-              ></textarea>
-              <div className="line"></div>
-            </div>
-            <button className="btn-main-offer contact-btn" type="submit">
-              Contact me
-            </button>
-          </div>
-        </div>
+        </form>
       </div>
     </div>
   );
