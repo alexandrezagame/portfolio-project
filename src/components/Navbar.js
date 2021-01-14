0f5de717c88c89438bd16e0039b68ac308743c6b
Alexandre Zagame <zagamealexandre@gmail.com>
2021-01-14 18:48:17 +0100

    cleaning code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import logo from "../logo.png";
 import "../stylesheets/Navbar.css";
 import {Link} from "react-scroll";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
