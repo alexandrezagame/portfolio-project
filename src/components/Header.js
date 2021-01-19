8b701a5ddd7e4935a1f818c2eab1bf08d44845c5
Alexandre Zagame <zagamealexandre@gmail.com>
2021-01-19 18:22:41 +0100

    adding custom font changing header and projects styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ import "../stylesheets/Header.css";
 import Typed from "react-typed";
 
 const Header = () => {
-
   const [offset, setOffset] = useState(0);
 
   useEffect(() => {
@@ -21,22 +20,21 @@ const Header = () => {
 
   return (
     <div id="home" className="header-wraper">
-
       <img
-          src="https://images.unsplash.com/photo-1609342122563-a43ac8917a3a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80"
-          alt="test"
-          className="parallax"
-          style={{
-            filter: `blur(4px)`,
-            transform: `translateY(${offset * 0.2}px)`
-          }}
-        />
+        src="https://images.unsplash.com/photo-1546931588-d7ae6f398f9b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80"
+        alt="test"
+        className="parallax"
+        style={{
+          filter: `blur(4px)`,
+          transform: `translateY(${offset * 0.2}px)`,
+        }}
+      />
 
       <div className="main-info">
         <h1>Alexandre Zagame</h1>
-        <Typed 
-          className="typed-text" 
-          strings={["Web Design", "Web Development", "Project Management"]} 
+        <Typed
+          className="typed-text"
+          strings={["Web Design", "Web Development", "Project Management"]}
           typeSpeed={40}
           backSpeed={60}
           loop
